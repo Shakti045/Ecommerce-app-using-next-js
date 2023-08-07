5d28af1ecbd7bdbc6ab77bbd057eadbcdecdaf78
Shakti045 <shaktimandal045@gmail.com>
2023-08-07 08:20:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+    const nextConfig = {
     images: {
-      domains: ["res.cloudinary.com"]
-    }
+    domains: ["res.cloudinary.com"]
+     },
+  async headers() {
+
+      return [
+          {
+              source: "/api/:path*",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "true" },
+                  { key: "Access-Control-Allow-Origin", value: "https://ecommerce-seller-nine.vercel.app" }, 
+                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                  { key: "Access-Control-Allow-Headers", value: "Content-Type,Authorization" },
+                  
+              ]
+          }
+      ]
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
