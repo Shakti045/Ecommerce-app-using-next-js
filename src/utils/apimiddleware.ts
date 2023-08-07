7413cae37eca73a0d47acbd7a4599801264d1ca5
Shakti045 <shaktimandal045@gmail.com>
2023-08-07 09:05:55 +0530

    Update apimiddleware.ts

--- src/utils/apimiddleware.ts
+++ src/utils/apimiddleware.ts
@@ -2,7 +2,7 @@ import { NextRequest,NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 export const getuserfromtoken=async(req:NextRequest)=>{
      try{
-        let token=req.cookies.get("ecommercetoken")?.value || req.headers.get("Authorization")?.replace("Bearer ","");
+        let token=req.headers.get("Authorization")?.replace("Bearer ","");
         if(token){
             const decodedtoken:any=jwt.verify(token,process.env.JWT_SECRET_KEY!);
             return decodedtoken;
