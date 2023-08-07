d006b783338483707f3658e8680f66c0240d2a96
Shakti045 <shaktimandal045@gmail.com>
2023-08-07 08:49:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@
               source: "/api/:path*",
               headers: [
                   { key: "Access-Control-Allow-Credentials", value: "true" },
-                  { key: "Access-Control-Allow-Origin", value: "https://ecommerce-seller-nine.vercel.app" }, 
+                  { key: "Access-Control-Allow-Origin", value: "https://ecommerce-seller-5u2lxm5l7-shakti045.vercel.app" }, 
                   { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                   { key: "Access-Control-Allow-Headers", value: "Content-Type,Authorization" },
                   
